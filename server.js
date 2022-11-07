d0ddfc442c7a40f82c9f96c409323ce296307828
Sharon Bello <sharonbellotech@gmail.com>
2022-11-07 20:30:27 +0200

    attempt config render deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3030;
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -9,9 +10,17 @@ const path = require('path');
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`)
+})
