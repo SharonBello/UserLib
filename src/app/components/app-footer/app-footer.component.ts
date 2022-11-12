4f680d58686a45dd86416fb1da58bf507be45916
Sharon Bello <sharonbellotech@gmail.com>
2022-11-12 13:05:33 +0200

    footer icons

--- src/app/components/app-footer/app-footer.component.ts
+++ src/app/components/app-footer/app-footer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome'
+import { fab, faFacebook, faInstagram, faTwitter, faLinkedin, faYoutube, faGithub, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
+
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +11,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppFooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fab);
+    library.addIcons(faFacebook,faTwitter,faInstagram,faLinkedin,faYoutube, faGithub, faWhatsapp);
+  }
 
   ngOnInit(): void {
   }
